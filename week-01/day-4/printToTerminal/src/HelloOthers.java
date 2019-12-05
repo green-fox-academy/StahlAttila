dd30395e6d1ba308d8b274c4e0d21d8ddd8d048e
Stahl <stattilasd@gmail.com>
2019-12-05 11:38:55 +0100

    added printToTerminal exercices

---
+++ week-01/day-4/printToTerminal/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dávid");
+        System.out.println("hello, Gergő");
+        System.out.println("Hello, other Dávid");
+    }
+}
