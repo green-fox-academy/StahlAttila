247f423d82924971f50207b4d467e3d59353d0eb
Stahl <stattilasd@gmail.com>
2019-12-05 13:43:56 +0100

    added DefineBasicInfo

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Stáhl Attila";
+        int myAge = 27;
+        double myHeight = 1.75;
+        boolean married = false;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(married);
+    }
+}
