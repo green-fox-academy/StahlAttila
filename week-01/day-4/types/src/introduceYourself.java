b521a784cda3eee4669d20b22aaf22b525d3c668
Stahl <stattilasd@gmail.com>
2019-12-05 11:47:19 +0100

    Added introduceYourself task

---
+++ week-01/day-4/types/src/introduceYourself.java
@@ -0,0 +1,10 @@
+public class introduceYourself {
+    public static void main(String[] args) {
+        String myName = "Stáhl Attila";
+        int myAge = 27;
+        double myHeight = 1.75;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+    }
+}
