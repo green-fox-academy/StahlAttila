5d08592323fa5c66cc6c0ad292dd6e7d8ff35c39
Stahl <stattilasd@gmail.com>
2019-12-06 17:36:02 +0100

    Added Substr

---
+++ week-01/day-5/src/SubStr.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SubStr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String q = scanner.nextLine();
+        public static int subStr(String input, String q) {
+            return -1;
+
+        String substr = input.substring();
+        System.out.println(substr);
+    }
+}
