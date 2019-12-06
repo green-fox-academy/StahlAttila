13a9fe481a45482c8d9842f9c97c1835bd1afc43
Stahl <stattilasd@gmail.com>
2019-12-06 10:44:17 +0100

    modified DrawTriangle

--- week-01/day-5/src/DrawTriangle.java
+++ week-01/day-5/src/DrawTriangle.java
@@ -5,8 +5,10 @@ public class DrawTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the height of the triangle:");
         int inputTriangLns = scanner.nextInt();
-        for (int i = 1; i <= inputTriangLns ; i++) {
-            for (int j = 1; j <= i; j++) {
+        //empty space to separate the number from the triangle
+        System.out.println("");
+        for (int i = 0; i <= inputTriangLns ; i++) {
+            for (int j = 0; j <= i; j++) {
                 //the empty spaces makes the triangle look like an even sided triangle
                 System.out.print(" ");
                 System.out.print("*");
