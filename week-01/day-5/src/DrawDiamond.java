17518a29bb4482efeac2054bb1d60aecf121ae48
Stahl <stattilasd@gmail.com>
2019-12-06 14:38:22 +0100

    modified DrawDiamond

--- week-01/day-5/src/DrawDiamond.java
+++ week-01/day-5/src/DrawDiamond.java
@@ -6,9 +6,16 @@ public class DrawDiamond {
         System.out.println("Enter the rows:");
         int inputRows = scanner.nextInt();
         for (int i = 1; i <= inputRows / 2 ; i++) {
-            for (int j = 1; j <= (inputRows/2) - i ; j++) {
-                System.out.print(" ");
+            //using an IF to push with 1 additional space when its an even row because of the additional row
+            if(inputRows % 2 == 0){
+                for (int j = 1; j <= (inputRows/2) - i ; j++) {
+                    System.out.print(" ");
+                }
 
+            }else {
+                for (int j = 1; j <= (inputRows/2)+1 - i ; j++) {
+                    System.out.print(" ");
+                }
             }
             for (int k = 1; k < 2 * i; k++) {
                 System.out.print("*");
@@ -16,18 +23,30 @@ public class DrawDiamond {
 
             System.out.println();
 
-        //prints out the pyramid upside down
-        //not working properly, printing out less rows
+        //prints out the pyramid upside down IF loop to check if its an even row or not
         }
-        for (int l = (inputRows/2) -1; l > 0 ; l--) {
-            for (int m = 0; m < (inputRows/2) - l; m++) {
-                System.out.print(" ");
+        if (inputRows % 2 == 0){
+            for (int l1 = (inputRows/2); l1 > 0 ; l1--) {
+                for (int m1 = 0; m1 < (inputRows / 2) - l1; m1++) {
+                    System.out.print(" ");
 
+                }
+                for (int n1 = 0; n1 < (2 * l1) - 1; n1++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            for (int n = 0; n < (2 * l) -1; n++) {
-                System.out.print("*");
+        }else {
+            for (int l1 = (inputRows/2) +1; l1 > 0 ; l1--) {
+                for (int m1 = 0; m1 < (inputRows / 2) + 1 - l1; m1++) {
+                    System.out.print(" ");
+
+                }
+                for (int n1 = 0; n1 < (2 * l1) - 1; n1++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
