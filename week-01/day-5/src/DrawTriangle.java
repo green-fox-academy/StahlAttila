b31438b6999075a17159af9a2ea46dd2edb512b1
Stahl <stattilasd@gmail.com>
2019-12-06 10:18:14 +0100

    modified DrawTriangle

--- week-01/day-5/src/DrawTriangle.java
+++ week-01/day-5/src/DrawTriangle.java
@@ -7,7 +7,10 @@ public class DrawTriangle {
         int inputTriangLns = scanner.nextInt();
         for (int i = 1; i <= inputTriangLns ; i++) {
             for (int j = 1; j <= i; j++) {
+                //the empty spaces makes the triangle look like an even sided triangle
+                System.out.print(" ");
                 System.out.print("*");
+                System.out.print(" ");
             }
             System.out.println("");
         }
