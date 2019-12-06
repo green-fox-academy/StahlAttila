02501aa26966d3e303730b41820540f98e55232f
Stahl <stattilasd@gmail.com>
2019-12-06 15:28:09 +0100

    modified ParameticAcerage

--- week-01/day-5/src/ParametricAverage.java
+++ week-01/day-5/src/ParametricAverage.java
@@ -6,14 +6,10 @@ public class ParametricAverage {
         System.out.println("Enter how many numbers you want the average and sum of:");
         int totalNums = scanner.nextInt();
         int sum = 0;
-        double avg = 0;
-        for (int i = 1; i <= totalNums; i++) {
-            System.out.println("Give me the "+ i + ". number:");
-            int nums = scanner.nextInt();
-            sum += nums;
-            avg = sum / totalNums;
+        for (int i = 0; i < totalNums; i++) {
+            System.out.println("Give me the "+ (i+1) + ". number:");
+            sum += scanner.nextInt();
         }
-
-        System.out.println("Sum: "+sum +", "+ "Average: "+ avg);
+        System.out.println("Sum: "+sum +", "+ "Average: "+ ((double)sum/totalNums));
     }
 }
