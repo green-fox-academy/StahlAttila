1b5410555c611ed9e065d33807be68cca464952f
Stahl <stattilasd@gmail.com>
2019-12-09 13:09:19 +0100

    add Greet

---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,9 @@
+public class Greet {
+    public static void main(String[] args) {
+        String a1 = "Green Fox";
+        greet(a1);
+    }
+    public static void greet(String text){
+        System.out.println("Greetings dear, " + text);
+    }
+}
