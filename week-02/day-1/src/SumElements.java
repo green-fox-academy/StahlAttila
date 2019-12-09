e725d2f29520a60cfa4a910cd996bbf957252047
Stahl <stattilasd@gmail.com>
2019-12-09 13:11:46 +0100

    add SumElements

---
+++ week-02/day-1/src/SumElements.java
@@ -0,0 +1,6 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int [] r = {54, 23, 66, 12};
+        System.out.println("The sum of the second and third element is: " +(r[1]+r[2]));
+    }
+}
