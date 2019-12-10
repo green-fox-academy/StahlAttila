20cb168c38ac97aea8582c31f4634b46bb2a4b16
Stahl <stattilasd@gmail.com>
2019-12-10 13:24:21 +0100

    add TakesLonger

---
+++ week-02/day-2/src/TakesLonger.java
@@ -0,0 +1,14 @@
+public class TakesLonger {
+    public static void main(String[] args) {
+        String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+        // When saving this quote a disk error has occured. Please fix it.
+        // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+        // Using pieces of the quote variable (instead of just redefining the string)
+        StringBuilder str = new StringBuilder();
+        str.append(quote.substring(0,21));
+        str.append("always takes longer than ");
+        str.append(quote.substring(23));
+        System.out.println(str);
+    }
+}
