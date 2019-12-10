b2492bfd8f7c2c81efaa4e8f03c1ef42c2331d97
Stahl <stattilasd@gmail.com>
2019-12-10 13:55:46 +0100

    modified ToDoPrint

--- week-02/day-2/src/ToDoPrint.java
+++ week-02/day-2/src/ToDoPrint.java
@@ -1,18 +1,16 @@
 public class ToDoPrint {
     public static void main(String[] args) {
         String todoText = " - Buy milk\n";
-        todoText ="My todo:\n" + todoText + " - Download games\n" + "    -Diablo";
-        // Add "My todo:" to the beginning of the todoText
-        // Add " - Download games" to the end of the todoText
-        // Add " - Diablo" to the end of the todoText but with indention
+        todoText ="My todo:\n" + todoText + " - Download games\n" + " \t-Diablo";
 
-        // Expected outpt:
-
-        // My todo:
-        //  - Buy milk
-        //  - Download games
-        //      - Diablo
-
+        //with Stringbuilder
+        StringBuilder todoSB = new StringBuilder();
+        todoSB.append("My todo:\n");
+        todoSB.append(todoText);
+        todoSB.append(" - Download games\n");
+        todoSB.append(" \t- Diablo");
+        //with Stringbuilder
+        //System.out.println(todoSB);
         System.out.println(todoText);
     }
 
