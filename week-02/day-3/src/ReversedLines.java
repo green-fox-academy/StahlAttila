777b4d9fe624f33378f4c64dfe9b983d3b8869d5
Stahl <stattilasd@gmail.com>
2019-12-11 22:42:40 +0100

    modified ReversedLines

--- week-02/day-3/src/ReversedLines.java
+++ week-02/day-3/src/ReversedLines.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,23 +15,20 @@ public class ReversedLines {
     private static void reversedLines(Path path) {
         try {
             List<String> lines = Files.readAllLines(path);
+            StringBuilder stringBuilder = new StringBuilder();
             //convert list to array
             String[] str = new String[lines.size()];
-            for (int i = 0; i < lines.size(); i++) {
+            for (int i = lines.size()-1; i >= 0; i--) {
                 str[i] = lines.get(i);
+                stringBuilder.append(str[i]);
+                stringBuilder.reverse();
             }
+            System.out.println(stringBuilder.toString());
             //Reversing the order of the lines
-            String temp;
-            for (int i = 0; i < lines.size() / 2; i++) {
-                temp = str[i];
-                str[i] = str[lines.size() - 1 - i];
-                str[lines.size() - 1 - i] = temp;
-            }
+
             //Added the lines to a new list
-            List<String> newLines = new ArrayList<>(Arrays.asList(str));
-            for (String item : newLines) {
-                System.out.println(item);
-            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
