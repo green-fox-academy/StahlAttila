f9864429e65e7395042914062ae9c3c4ceda7fd2
Stahl <stattilasd@gmail.com>
2019-12-13 10:19:49 +0100

    modified Log

--- week-02/day-3/src/Log.java
+++ week-02/day-3/src/Log.java
@@ -1,46 +1,47 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Log {
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("src/log.txt");
-        System.out.println("The uniqe IP addresses: " + uniqueIP(path));
-        System.out.println("The GET/POST ratio is: " + getPost(path));
+    public static void main(String[] args) {
+        try {
+            Path path = Paths.get("src/log.txt");
+            System.out.println("The uniqe IP addresses: " + uniqueIP(path));
+            System.out.println("The GET/POST ratio is: " + getPostRatio(path));
+        } catch (NoSuchFileException e) {
+            System.out.println("No such file found.");
+        } catch (IOException e) {
+            System.out.println("Something went wrong.");
+        }
     }
 
-    private static ArrayList<String> uniqueIP(Path path) {
+    private static ArrayList<String> uniqueIP(Path path) throws IOException {
         ArrayList<String> ipAddresses = new ArrayList<>();
-        try {
-            List<String> lines = Files.readAllLines(path);
-            for (String line : lines) {
-                if (!ipAddresses.contains(line.substring(27, 39))) {
-                    ipAddresses.add(line.substring(27, 39));
-                }
+        List<String> lines = Files.readAllLines(path);
+        for (String line : lines) {
+            if (!ipAddresses.contains(line.substring(27, 38))) {
+                ipAddresses.add(line.substring(27, 38));
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
         return ipAddresses;
     }
 
-    public static Double getPost(Path path) throws IOException {
+    public static Double getPostRatio(Path path) throws IOException {
         List<String> lines = Files.readAllLines(path);
-        Integer counterGet = 0;
-        Integer counterPost = 0;
+        int counterGet = 0;
+        int counterPost = 0;
         for (String line : lines) {
             if (line.contains("GET")) {
                 counterGet++;
             } else {
                 counterPost++;
             }
-
         }
-        return counterGet.doubleValue()/counterPost;
+        return (double) counterGet / counterPost;
     }
-    
 }
 
